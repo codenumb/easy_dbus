7c1bc8a4a32586309ce8ffa34c2f9681365a3892
codenumb <thejaskoot@gmail.com>
2022-10-19 11:44:11 +0530

    add code comments in header files

--- include/dbus-client.h
+++ include/dbus-client.h
@@ -10,32 +10,53 @@ typedef struct method_reply
 	void *data;
 	int len;
 }method_reply_t;
-/* 
- * return:   0 on success, -1 on failure.
+
+/**
+ * Function to initialize the dbus_client library.which establish a connection with the dbus bus.
+ * @param appname the unique application name.other processes use this name to communicate.
+ * @return 0 on success otherwise error code.
  */
 extern int dbus_client_init (char *appname);
 
-/* This API subscribes for a particular busname/interface/method. The callback would
- * be called once the subscribed information is available.
- *Note: subscribe = listen => server bus name (e.g., mqtt.server.cmdhndlr).
- *
- * method:    method name you wnat to listen to.
- * cb:        callback to be called once the subscribed information is available.
- * return:    0 on success, -1 on failure.
+/**
+ * Use this function to subscribe signals or methods. pass method/signals and
+ * call back functions as comma separated pairs.list of pairs can be given with comma 
+ * separation. 
+ * eg  dbus_client_subscribe(3,"sig1",sig1_callback,"sig2",sig2_callback,"method1",method1_callback).
+ * @param num_pairs number of pairs.
+ * @return 0 on success, -1 on failure.
  */
-
 extern int dbus_client_subscribe (int num_pairs, ...);
 
-/* 
- * method:     method name
- * payload:    data that needs to be sent/published.
- * payloadlen: length of payload in bytes.
- * return:     0 on success, -1 on failure.
+/**
+ * Function to broadcast a signal.
+ * @param method name of the signal.
+ * @param payload data to be passed to listeners.
+ * @param payloadlen length of the data in bytes.
+ * @return 0 on success, -1 on failure.
  */
 extern int dbus_client_publish (char *method, 
                                 void *payload,
                                 int payloadlen);
+
+/**
+ * Use this function to initialize the dbus method reply buffer.
+ * no need to call de-init.
+ * @param len required buffer legth in bytes.
+ * @return -ve error code on failure and method_reply on success.
+ */
 extern method_reply_t *dbus_client_init_reply_buff(int len);
+
+/**
+ * Wrapper to call a method  of a remote process.This function is a blocking call.
+ * the remote method must respond with a return value.
+ * @param appname name of the remote process bus name.
+ * @param method remote method to be invoked
+ * @param payload argument to be passed to the remote method.
+ * @param payloadlen length of the argument
+ * @param reply buffer to store the reply from remote method.
+ * @return -ve error code on failure and 0 on success.
+ */
 extern int  dbus_client_call_remote_method_with_reply(char *busname,
 													  char *method,
 													  void *payload,
